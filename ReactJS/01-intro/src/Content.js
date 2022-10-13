cc29f1bb3712441f45c9d0fd296e0f46e4f76992
Khalid <khaltahan@gmail.com>
2022-10-13 17:55:11 -0400

    tutorial 5

--- ReactJS/01-intro/src/Content.js
+++ ReactJS/01-intro/src/Content.js
@@ -1,14 +1,29 @@
 const Content = () => {
   const handleNameChange = () => {
-    const names = ["Bobs", "Kevin", "Khalid"];
+    const names = ["Bob", "Kevin", "Khalid"];
     // Gets a number between 0 and 2
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
+  // Notice how we do not add () to the onClick functions
+  // We do that so the function are not called on load, and are only called on click
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2("Dave")}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   );
 };
